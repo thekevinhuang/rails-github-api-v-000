5d639e78d13514909637f0ca2287612875453039
Kevin Huang <yellow.khaos@gmail.com>
2018-12-02 16:44:26 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,12 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.params[:access_token] = session[:token]
+    end
+    binding.pry
+    @info = JSON.parse(resp.body)
+
   end
 
 end
