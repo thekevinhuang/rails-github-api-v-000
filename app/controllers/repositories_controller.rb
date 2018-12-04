8b73f8ad7c5bf2a3281f2c8b5336e716393bb707
Kevin Huang <yellow.khaos@gmail.com>
2018-12-04 03:26:05 +0000

    Automatically backed up by Learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
+    new_params = CGI::parse(session[:token])
+    binding.pry
+    resp = Faraday.get("https://api.github.com/user/repos") do |req|
+      req.params['access_token'] = new_params["access_token"]
+      req.params['scope'] =  ""
+      req.params['token_type'] = new_params["token_type"]
+    end
+    binding.pry
+    @info = JSON.parse(resp.body)
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,13 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
+    resp = Faraday.post("https://github.com/login/oauth/access_token") do |req|
+      req.params['client_id'] = ENV['GITHUB_CLIENT_ID']
+      req.params['client_secret'] = ENV['GITHUB_CLIENT_SECRET']
+      req.params['code'] = params[:code]
+      req.headers['Content-Type'] = 'application/json'
+    end
+    session[:token] = resp.body
+    redirect_to root_path
   end
 end
